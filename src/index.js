48d97d442d9e9ccb8fb6e4e028f37cf80aa44261
Stefan Prosiegel <stefan.prosiegel@googlemail.com>
2019-08-18 16:15:47 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-const teams = () => ["a", "b"];
-export default { teams };
+import * as R from "ramda";
+
+const generateFixtures = teams => {
+  const indexedTeams = R.zip(R.range(1, R.length(teams) + 1), teams);
+  const regularIndexedTeams = R.dropLast(1, indexedTeams);
+  const lastIndexedTeam = R.takeLast(1, indexedTeams);
+  console.log(regularIndexedTeams);
+  console.log(lastIndexedTeam);
+};
+
+export default generateFixtures;
