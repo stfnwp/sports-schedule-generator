5a04a2b21d4d01fe46c095622f4463f43dcf0577
Stefan Prosiegel <stefan.prosiegel@googlemail.com>
2019-08-18 16:15:47 +0200

    wip

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,12 @@
-import foo from "./index";
+import generateFixtures from "./index";
 
 describe("basic", () => {
   it("works", () => {
-    expect(foo.teams()).toHaveLength(2);
+    const expected = [
+      { matchday: 1, matches: [{ homeTeam: "a", awayTeam: "b" }, { homeTeam: "c", awayTeam: "d" }] },
+      { matchday: 2, matches: [{ homeTeam: "a", awayTeam: "d" }, { homeTeam: "c", awayTeam: "b" }] },
+      { matchday: 3, matches: [{ homeTeam: "a", awayTeam: "c" }, { homeTeam: "b", awayTeam: "d" }] }
+    ];
+    expect(generateFixtures(["a", "b", "c", "d"])).toStrictEqual(expected);
   });
 });
